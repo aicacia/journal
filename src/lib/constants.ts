8fdb63ae7d4152c9ce95eccdbd89f6431a989cc4
Nathan Faucett <nathanfaucett@gmail.com>
2022-07-06 07:57:56 -0400

    fix api keys

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const DROPBOX_CLIENT_ID = 'v21wdidfxjapm8b';
+export const DROPBOX_CLIENT_ID = 'lf5maxp3ghh4q1s';
 export const GOOGLE_DRIVE_CLIENT_ID =
-	'399181883347-3dfijgbemptplmueu01bsfga9jpkks6j.apps.googleusercontent.com';
+	'220951148444-5u4fja36i0dm85hcvipq6udm9msutf7n.apps.googleusercontent.com';
