8a558b76e8c335b664c25055eb4a22a69b15b57b
Nathan Faucett <nathanfaucett@gmail.com>
2022-07-24 10:06:06 -0400

    update packages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
+	server: {
+		port: 3000
+	},
+	preview: {
+		port: 3000
+	},
+	ssr: {
+		noExternal: [/^svelte-slate?\//]
+	},
 	plugins: [sveltekit()]
 };
 
